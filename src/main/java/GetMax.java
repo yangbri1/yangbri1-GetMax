74ac86472178e32141a3e0d07e15034264336316
Brian Yang <yangbri1@msu.edu>
2024-12-05 08:39:04 +0000

    looping through a given array & trying to find the largest integer element

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -1,3 +1,5 @@
+// import java.util.Array;
+
 public class GetMax {
     /**
      * This method should return the max value of arr (the largest int.)
@@ -7,6 +9,29 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        
+        // sadly java.util.Array do NOT seem to want to import ...
+        /* 1 way could be to use Array.sort() method to sort 'arr' into ascending order  & then retrieve last element (largest number) */
+        // Array.sort(arr);   
+        // return(arr[arr.length - 1]);
+        
+        // initialize variable 'bigNum' to be Java's int lower bound (no 'signed' integer could be lower than this w/o 'underflow') & wrap around
+        // Recall: int data type in Java lower bound is -2,147,483,648
+        int bigNum = -2147483648;
+
+        // initialize iterator to 0
+        int itr = 0;
+        // loop via array 'arr' ...
+        while(itr < arr.length){
+            // if value at current iterated index is bigger than 'bigNum' ...
+            if(arr[itr] > bigNum){
+                // assign value to 'bigNum'
+                bigNum = arr[itr];
+            }
+            // increment by 1 to continue iteration
+            ++itr;
+        }
+        /
+        return bigNum;
     }
 }
